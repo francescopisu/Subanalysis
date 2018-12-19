131fea948820494f96c966739f9d9b44d3103644
francescopisu <frapisu9@gmail.com>
2018-12-19 19:25:39 +0100

    changes
    - output file opened when the script starts
    - output file line's structure:
    series, season, episode, w/h
    - lines starting with \ are not counted
    - it is necessary to sort the output of os.listdir in an alphabetical fashion, otherwise the ordering will depends on the filesystem

--- srt_handler.py
+++ srt_handler.py
@@ -2,41 +2,47 @@ import os
 import re
 
 
+
 def compute_wh():
-    # run through each subdirectory of subs folder (i.e: tv series)
-    for subdir_first_level in next(os.walk('subs'))[1]:
-        current_dir = 'subs/' + subdir_first_level
+    # opening the results file
+    with open('results.txt', "w+") as resFile:
+        # run through each subdirectory of subs folder (i.e: tv series)
+        for subdir_first_level in sorted(next(os.walk('subs'))[1]):
+            current_dir = 'subs/' + subdir_first_level
+            # gather series number
+            series = subdir_first_level[:2]
 
-        # run through each subdirectory of series folders (i.e: seasons)
-        for subdir_second_level in os.listdir(current_dir):
-            if not subdir_second_level.startswith('.'):
-                snd_current_dir = current_dir + '/' + subdir_second_level
+            # run through each subdirectory of series folders (i.e: seasons)
+            for subdir_second_level in sorted(os.listdir(current_dir)):
+                if not subdir_second_level.startswith('.'):
+                    snd_current_dir = current_dir + '/' + subdir_second_level
+                    # gather season number
+                    season = subdir_second_level[2]
 
-                # save the results for each season
-                with open(snd_current_dir + '/results.txt', "w+") as resFile:
-                    # operate on each season's file
-                    for subFile in os.listdir(snd_current_dir):
+                    # operate on each season's episode
+                    episode = 0
+                    for subFile in sorted(os.listdir(snd_current_dir)):
                         if not subFile.startswith('.'):
                             if subFile.endswith(".srt"):
                                 with open(snd_current_dir + '/' + subFile, "r") as f:
+                                    episode += 1
                                     text = []
-                                    words_count = 0
                                     for line in f.readlines():
                                         if not line[0].isdigit():
                                             if not re.match(r'^\s*$', line):
                                                 text.append(line.rstrip('\r\n'))
                                     # text must be cleared of punctuation and other symbols
                                     words_count = count_words(text)
-                                    resFile.write(str(words_count))
-                                    resFile.write(',')
-                                    resFile.write(str(words_count / float(60)))
+                                    resFile.write('series, season, episode, w/h')
+                                    resFile.write(series + ',' + str(season) + ',' + str(episode) + ',' + str(words_count / float(60)))
                                     resFile.write('\n')
 
 
 def count_words(list_of_strings):
     count = 0
     for line in list_of_strings:
-        if line[0].isalpha():
+        # the sub line is not counted if it starts with \
+        if line[0] != '\\':
             count += len(re.findall(r'\w+', line))
 
     return count
