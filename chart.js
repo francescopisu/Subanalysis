7c63a7dfaeb38205178706f7ad71add02e1dab2d
carlocabras21 <karlokabras@gmail.com>
2019-03-12 17:17:04 +0100

    Update chart.js

--- chart.js
+++ chart.js
@@ -5,7 +5,12 @@ class Chart {
         this.element = opts.element;
         //console.log(this.data)
 
-        this.zoomLevel = 3;
+        this.zoomLevel = 2;
+
+        this.extractElements();
+        // console.log(this.series);
+        // console.log(this.seasons);
+        // console.log(this.episodes);
 
         // create the chart
         this.draw();
@@ -32,7 +37,7 @@ class Chart {
         this.x = d3.scale.ordinal().rangeBands([0, this.width], .05);
         this.y = d3.scale.linear().range([this.height, 0]);
 
-        this.x.domain(this.episodes.map(item => item.id))
+        this.x.domain(this.getCurrentData().map(item => item.id))
         this.y.domain([0,13000])
     }
 
@@ -78,7 +83,7 @@ class Chart {
            .attr("transform",
                 "translate(" + this.margin.left + "," + this.margin.top + ")");
 
-        
+
         this.svgChart.append("g")
             .attr("class", "x axis")
             .attr("transform", "translate(0," + this.height + ")")
@@ -86,99 +91,104 @@ class Chart {
             .selectAll("text")
             .style("text-anchor", "middle")
             .attr("dy", ".9em");
-    }   
-
-    addLine() {
-        const line = d3.line()
-            .x(d => this.xScale(d[0]))
-            .y(d => this.yScale(d[1]));
-
-        this.plot.append('path')
-            // use data stored in `this`
-            .datum(this.data)
-            .classed('line',true)
-            .attr('d',line)
-            // set stroke to specified color, or default to red
-            .style('stroke', this.lineColor || 'red');
     }
+
 
     addBars() {
+        var x = this.x;
+        var y = this.y;
+        var height = this.height;
+
         // Add bar chart
-        this.bars = svg.selectAll("bar")
-                      .data(this.episodes)
+        this.bars = this.svgChart.selectAll("bar")
+                      .data(this.getCurrentData())
                       .enter();
 
-    // w/h bars
-    this.bars.append("rect")
-      .attr("class", "bar")
-      .attr("x", function(item) { return x(item.id); })
-      .attr("width", x.rangeBand())
-      .attr("y", function(item) { return y(item.wh); })
-      .attr("height", function(item) { return height - y(item.wh); });
+        // w/h bars
+        this.bars.append("rect")
+          .attr("class", "bar")
+          .attr("x", function(item) { return x(item.id); })
+          .attr("width", x.rangeBand())
+          .attr("y", function(item) { return y(item.wh); })
+          .attr("height", function(item) { return height - y(item.wh); });
 
-    // average w/h line
-    this.bars.append("rect")
-        .attr("class", "bar_line")
-        .attr("x", function(item) { return x(item.id); })
-        .attr("width", x.rangeBand() + 2)
-        .attr("y", function(item) { return y(data[item.series].avg_wh); })
-        .attr("height", function(item) { return 1; });
+        if (this.zoomLevel > 1) {
+            // average w/h line
+            var series = this.series;
+            this.bars.append("rect")
+                .attr("class", "bar_line")
+                .attr("x", function(item) { return x(item.id); })
+                .attr("width", x.rangeBand() + 2)
+                .attr("y", function(item) {
+                    return y(series[item.series].wh); })
+                .attr("height", function(item) { return 1; });
+        }
     }
 
-    getNumberOfElements(){
+    extractElements(){
         var i = 0;
-        if (this.zoomLevel == 1){
-            // zoom level 1: only series are shown
-            var series = [];
-            this.data.forEach(single_series => {
-                series.push({
+
+        var series = [];
+        this.data.forEach(single_series => {
+            series.push({
+                id: i++,
+                number: single_series.id_,
+                wh: single_series.avg_wh,
+                series: single_series.id_
+            })
+        });
+        this.series = series;
+
+        i = 0;
+        var seasons = [];
+        this.data.forEach(series => {
+            series.seasons.forEach(season => {
+                  seasons.push({
                     id: i++,
-                    number: single_series.id_,
-                    wh: single_series.avg_wh,
-                    series: single_series.id_
-                })
+                    number: season.id_,
+                    wh: season.avg_wh,
+                    series: series.id_
+                  })
             });
+        });
+        this.seasons = seasons;
 
-            this.series = series;
-            return series.length;
-        }
-
-        else if (this.zoomLevel == 2) {
-            // zoom level 2: only seasons are shown
-            var seasons = [];
-            this.data.forEach(series => {
-                series.seasons.forEach(season => {
-                      seasons.push({
+        i = 0;
+        var episodes = [];
+        this.data.forEach(series => {
+            series.seasons.forEach(season => {
+                season.episodes.forEach(episode => {
+                    episodes.push({
                         id: i++,
-                        number: season.id_,
-                        wh: season.avg_wh,
+                        number: episode.id_,
+                        wh: episode.wh,
+                        season: season.id_,
                         series: series.id_
-                      })
+                    })
                 });
             });
+        });
+        this.episodes = episodes;
+    }
 
-            this.seasons = seasons;
-            return seasons.length;
-        }
-        else { // zoomLevel == 3, only episodes are shown
-            var episodes = [];
-            this.data.forEach(series => {
-                series.seasons.forEach(season => {
-                    season.episodes.forEach(episode => {
-                        episodes.push({
-                            id: i++,
-                            number: episode.id_,
-                            wh: episode.wh,
-                            season: season.id_,
-                            series: series.id_
-                        })
-                    });
-                });
-            });
 
-            this.episodes = episodes;
-            return episodes.length;
-        }
+    getNumberOfElements(){
+        if (this.zoomLevel == 1)
+            return this.series.length;
+        else if (this.zoomLevel == 2)
+            return this.seasons.length;
+        else
+            return this.episodes.length;
+
+    }
+
+    getCurrentData(){
+        if (this.zoomLevel == 1)
+            return this.series;
+        else if (this.zoomLevel == 2)
+            return this.seasons;
+        else
+            return this.episodes;
     }
 
 }
