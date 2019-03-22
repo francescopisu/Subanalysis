157ca1b77cd823cf9427925b95e3bca7533e6c40
Carlo Cabras <karlokabras@gmail.com>
2019-03-22 11:49:29 +0100

    year fixed in specs

--- utils.py
+++ utils.py
@@ -41,7 +41,7 @@ def extract_series_data_from_specs(specs):
         current_series.name = row['name']
         current_series.episode_length = int(row['length'])
         current_series.genre = row['genre']
-        current_series.year = int(row['year'])
+        current_series.year = row['year']
 
     return current_series
 
